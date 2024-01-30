6d1a776c58526df856368ce14ca41872a9315680
Cadú <139708523+mrcadu7@users.noreply.github.com>
2024-01-30 18:28:14 -0300

    Refactor

--- dcelery/newapp/tasks.py
+++ dcelery/newapp/tasks.py
@@ -1,5 +1,25 @@
 from celery import shared_task
+import time
+
+@shared_task(task_rate_limit='1/m')
+def tp1(queue = 'celery'):
+    time.sleep(3)
+    return
+
 
 @shared_task
-def sharedtask():
+def tp2(queue = 'celery:1'):
+    time.sleep(3)
+    return
+
+
+@shared_task
+def tp3(queue = 'celery:2'):
+    time.sleep(3)
+    return
+
+
+@shared_task
+def tp4(queue = 'celery:3'):
+    time.sleep(3)
     return
